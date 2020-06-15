3f4c02999609203e281189105ec0b8fd26e4d645
kamataryo <kamataryo@users.noreply.github.com>
2020-06-16 01:43:51 +0900

    Show width

--- main.js
+++ main.js
@@ -8,8 +8,6 @@ const refresh = () => {
   let { lng: lng3, lat: lat3 } = bounds.getNorthEast();
   let { lng: lng4, lat: lat4 } = bounds.getSouthWest();
 
-  console.log({ lng1, lng2 });
-
   const [x1, y1] = tilebelt.pointToTile(lng1, lat1, zoom);
   const [x2, y2] = tilebelt.pointToTile(lng2, lat2, zoom);
   const [x3, y3] = tilebelt.pointToTile(lng3, lat3, zoom);
@@ -27,16 +25,35 @@ const refresh = () => {
 
   for (let x = min_x; x < max_x + 1; x++) {
     for (let y = min_y; y < max_y + 1; y++) {
+      const geometry = tilebelt.tileToGeoJSON([x, y, zoom]);
+      let l1 = turf.distance(
+        geometry.coordinates[0][0],
+        geometry.coordinates[0][1]
+      );
+      let l2 = turf.distance(
+        geometry.coordinates[0][1],
+        geometry.coordinates[0][2]
+      );
+      let unit = "km";
+      if (l1 > 1 && l2 > 1) {
+        l1 = (Math.round(l1 * 100) / 100).toString();
+        l2 = (Math.round(l2 * 100) / 100).toString();
+      } else {
+        l1 = Math.round(1000 * l1).toString();
+        l2 = Math.round(1000 * l2).toString();
+        unit = "m";
+      }
       geojson.features.push({
         type: "Feature",
-        properties: { x, y, z: zoom },
-        geometry: tilebelt.tileToGeoJSON([x, y, zoom]),
+        properties: { x, y, z: zoom, l1, l2, unit },
+        geometry,
       });
     }
   }
   try {
     map.removeLayer("grids-line");
-    map.removeLayer("grids-symbol");
+    map.removeLayer("grids-symbol-xyz");
+    map.removeLayer("grids-symbol-length");
     map.removeSource("grids");
   } catch (error) {
     // nothing
@@ -54,7 +71,7 @@ const refresh = () => {
     },
   });
   map.addLayer({
-    id: "grids-symbol",
+    id: "grids-symbol-xyz",
     source: "grids",
     type: "symbol",
     layout: {
@@ -67,6 +84,26 @@ const refresh = () => {
         ["get", "y"],
       ],
       "text-size": 16,
+      "text-font": ["Noto Sans Regular"],
+    },
+  });
+  map.addLayer({
+    id: "grids-symbol-length",
+    source: "grids",
+    type: "symbol",
+    layout: {
+      "text-field": [
+        "concat",
+        ["get", "l1"],
+        " ",
+        ["get", "unit"],
+        " x ",
+        ["get", "l2"],
+        " ",
+        ["get", "unit"],
+      ],
+      "text-size": 16,
+      "text-offset": [0, 3],
       "text-font": ["Noto Sans Regular"],
     },
   });
